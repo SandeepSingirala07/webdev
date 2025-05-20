8f1b220c6464a784dc08563c0eb24337df7491c8
SandeepSingirala07 <sandeepsk9876543@gmail.com>
2025-05-20 14:22:36 -0400

    directives3

--- angular_project_may_2025/src/app/directives/numberonly.directive.ts
+++ angular_project_may_2025/src/app/directives/numberonly.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appNumberonly]'
 })
 export class NumberonlyDirective {
 
-  constructor() { }
+
+    @HostBinding('style.background-color')
+  myBgColor: string = '';
+
+  @HostListener('keyup', ['$event.target.value'])
+  handleKeyUp(value: string) {
+    let regex = new RegExp(/^[0-9]*$/);
+    if (!regex.test(value)) {
+      this.myBgColor = 'red';
+    } else {
+      this.myBgColor = 'cyan';
+    }
+  }
 
 }
