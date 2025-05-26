17911ce98bdc7e3df393355a8fa91cbb3823b263
SandeepSingirala07 <sandeepsk9876543@gmail.com>
2025-05-26 11:54:03 -0400

    Angular

--- angular_project_may_2025/src/app/components/http-demo1/http-demo1.component.ts
+++ angular_project_may_2025/src/app/components/http-demo1/http-demo1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http'; 
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-http-demo1',
@@ -8,19 +9,44 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './http-demo1.component.css'
 })
 export class HttpDemo1Component {
+  user_url = 'https://jsonplaceholder.typicode.com/users';
+  user_subscription: any;
+  subscriptionArr: Subscription[] = [];
 
-  user_url = 'https://jsonplaceholder.typicode.com/users'
+  isLoading: boolean = false;
 
-  constructor(private httpClient: HttpClient){
-
+  constructor(private httpClient: HttpClient) {
   }
   ngOnInit() {
-    this.fectchUserData_angular();
+    // this.fetchUserData_javascript();
+    this.fetchUserData_angular();
+  }
+  fetchUserData_javascript() {
+    fetch(this.user_url).then(response => { // fetch() returns promise
+      response.json().then(finalResponse => {
+        console.log(finalResponse);
+      });
+    });
   }
 
-  fectchUserData_angular() {
-    this.httpClient.get(this.user_url).subscribe(response => {
+  fetchUserData_angular() { // httpClient.get() returns Observable
+    this.isLoading = true;
+    this.user_subscription = this.httpClient.get(this.user_url, { observe: 'response' }).subscribe(response => {
       console.log(response);
+      this.isLoading = false;
     })
+    this.subscriptionArr.push(this.user_subscription);
+  }
+
+  ngOnDestroy() {
+    if (this.user_subscription) {
+      this.user_subscription.unsubscribe();
+    }
+
+    if (this.subscriptionArr.length) {
+      this.subscriptionArr.forEach(ele => {
+        ele.unsubscribe();
+      })
+    }
   }
 }
